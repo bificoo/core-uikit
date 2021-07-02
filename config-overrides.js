1640b9f53083a8d4efe58a672c46582dc9139c25
wadezhu <wade@inntech.me>
2021-07-02 16:14:14 +0800

    Build uikit project and export components

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,14 @@
-module.exports = function override(config, env) {
-  config.output = {
-    ...config.output, // copy all settings
-    filename: "static/js/[name].js",
-    chunkFilename: "static/js/[name].chunk.js",
-  }
-  return config
+// const rewireEntry = require("rewire-entry")({
+//   entry: ["desktop.tsx", "touch.tsx"],
+// })
+
+module.exports = {
+  webpack: (config, env) => {
+    config.output = {
+      ...config.output, // copy all settings
+      filename: "static/js/[name].js",
+      chunkFilename: "static/js/[name].chunk.js",
+    }
+    return config
+  },
 }
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Badge } from "components/Badge"
+export { default as Button } from "components/Button"
+export { default as Dropdown } from "components/Dropdown"
+export { default as Form } from "components/Form"
+export { default as Tooltip } from "components/Tooltip"

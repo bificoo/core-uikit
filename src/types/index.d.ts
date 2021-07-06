5897d7e675806c8948252edcbeb39710a99b2a34
wadezhu <wade@inntech.me>
2021-07-06 19:39:27 +0800

    Modify index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,13 +1,20 @@
-export { default as Badge } from "components/Badge"
-export { default as Button } from "components/Button"
-export { default as Dropdown } from "components/Dropdown"
-export { default as Form } from "components/Form"
-export { default as Tooltip } from "components/Tooltip"
+// export * as Badge from "components/Badge"
 
-export type { ButtonProps } from "components/Button"
-export type { LinkButtonProps } from "components/Button/LinkButton"
+// export { default as Dropdown } from "components/Dropdown"
+// export { default as Form } from "components/Form"
+// export { default as Tooltip } from "components/Tooltip"
 
-export type { DropdownProps } from "components/Dropdown"
-export type { DropdownToggleProps } from "components/Dropdown/DropdownToggle"
-export type { DropdownMenuProps } from "components/Dropdown/DropdownMenu"
-export type { DropdownItemProps } from "components/Dropdown/DropdownItem"
+// export type { LinkButtonProps } from "components/Button/LinkButton"
+
+// export type { DropdownProps } from "components/Dropdown"
+// export type { DropdownToggleProps } from "components/Dropdown/DropdownToggle"
+// export type { DropdownMenuProps } from "components/Dropdown/DropdownMenu"
+// export type { DropdownItemProps } from "components/Dropdown/DropdownItem"
+
+declare module "core-uikit" {
+  import { ButtonProps } from "components/Button"
+  import { default as Button } from "components/Button"
+
+  declare const Button: React.Component<ButtonProps>
+  export default Button
+}

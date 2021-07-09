67bc461c2c1cf8b8220f31a98332f013104a0d74
wadezhu <wade@inntech.me>
2021-07-09 15:46:12 +0800

    Modify setting

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
-// const rewireEntry = require("rewire-entry")({
-//   entry: ["desktop.tsx", "touch.tsx"],
-// })
-
 module.exports = {
   webpack: (config, env) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+
     config.output = {
       ...config.output, // copy all settings
       filename: "static/js/[name].js",

834c5cd74b2cc0ead62ca5b656ddf113b5dc37a6
Bificoo <bificoo@gmail.com>
2021-07-16 21:11:14 +0800

    Test rollup for component tip - version4

--- rollup.config.js
+++ rollup.config.js
@@ -5,26 +5,12 @@ import typescript from "rollup-plugin-typescript2"
 import postcss from "rollup-plugin-postcss"
 import svgr from "@svgr/rollup"
 import url from "@rollup/plugin-url"
-// import rename from "rollup-plugin-rename"
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const packageJson = require("./package.json")
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path")
 
 export default {
   input: "src/index.ts",
-  // output: [
-  //   {
-  //     file: packageJson.main,
-  //     format: "cjs",
-  //     sourcemap: true,
-  //   },
-  //   {
-  //     file: packageJson.module,
-  //     format: "esm",
-  //     sourcemap: true,
-  //   },
-  // ],
   output: {
     dir: "build",
     format: "cjs",
@@ -51,22 +37,5 @@ export default {
     }),
     url(),
     svgr(),
-    // rename({
-    //   include: ["**/*.ts"],
-    //   map: name => {
-    //     console.info(
-    //       "---",
-    //       name,
-    //       name
-    //         .replace("src/", "")
-    //         .replace("node_modules/", "external/")
-    //         .replace("../../external", "../external"),
-    //     )
-    //     return name
-    //       .replace("src/", "")
-    //       .replace("node_modules/", "external/")
-    //       .replace("../../external", "../external")
-    //   },
-    // }),
   ],
 }

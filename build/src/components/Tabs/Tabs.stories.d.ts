6d14814f0812ebf1cf893644fefdb1cb806a1bcb
peggy.shi <peggys@innotech.me>
2021-08-05 15:23:43 +0800

    feat(UT-291): optimize tabs style

--- build/src/components/Tabs/Tabs.stories.d.ts
+++ build/src/components/Tabs/Tabs.stories.d.ts
@@ -3,3 +3,4 @@ import { TabsProps } from "components/Tabs";
 declare const _default: Meta<import("@storybook/react").Args>;
 export default _default;
 export declare const Default: Story<TabsProps>;
+export declare const NoTabContent: Story<TabsProps>;
--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -47,3 +47,34 @@ export const Default: Story<TabsProps> = () => {
     </Tabs>
   )
 }
+
+export const NoTabContent: Story<TabsProps> = () => {
+  const style = {
+    alignItems: "center",
+    backgroundColor: "#f4f5f7",
+    borderRadius: "3px",
+    color: "#6b778c",
+    display: "flex",
+    fontSize: "4em",
+    fontWeight: 500,
+    flexGrow: 1,
+    justifyContent: "center",
+    padding: "32px",
+  }
+
+  return (
+    <>
+      <Tabs
+        defaultActiveKey="Tab1"
+        onSelect={(
+          e: React.MouseEvent<Element, MouseEvent>,
+          { eventKey }: { eventKey?: ReactProps.EventKey },
+        ) => console.info(eventKey)}>
+        <Tabs.Tab title="數據指標" eventKey="Tab1" />
+        <Tabs.Tab title="圖像報表" eventKey="Tab2" />
+        <Tabs.Tab title="用戶清單" eventKey="Tab3" />
+      </Tabs>
+      <div style={{ background: "#ccc" }}>Content</div>
+    </>
+  )
+}

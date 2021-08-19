cfda9e31b9f40fc7754d370f8b492c0596d85ae3
peggy.shi <peggys@innotech.me>
2021-08-19 15:57:29 +0800

    feat(UT-290): fix build

--- build/src/components/DatePicker/index.d.ts
+++ build/src/components/DatePicker/index.d.ts
@@ -3,7 +3,5 @@ import DatePicker from "./DatePicker";
 export type { DatePickerProps } from "./DatePicker";
 import RangePicker from "./RangePicker";
 export type { RangePickerProps } from "./RangePicker";
-import DateFormControl from "./DateFormControl";
-export type { DateFormControlProps } from "./DateFormControl";
 export default DatePicker;
-export { RangePicker, DateFormControl };
+export { RangePicker };

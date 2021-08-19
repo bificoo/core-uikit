67a6f6d4ad435ff5478fbf02a3641ab783c5d6e3
peggy.shi <peggys@innotech.me>
2021-08-19 11:54:48 +0800

    feat(UT-290): re-build

--- build/src/components/Tooltip/Tooltip/Tooltip.scss.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var styleInject_es = require('../../../../node_modules/style-inject/dist/style-inject.es.js');
-
-var css_248z = "@keyframes Tooltip_anvil__2Vibk {\n  0% {\n    opacity: 0;\n    box-shadow: 0 0 0 rgba(241, 241, 241, 0); }\n  1% {\n    opacity: 0;\n    box-shadow: 0 0 0 rgba(241, 241, 241, 0); }\n  100% {\n    opacity: 1;\n    box-shadow: 0 0 500px rgba(241, 241, 241, 0); } }\n\n.Tooltip_core-tooltip-arrow__SnQRY {\n  color: #172b4d; }\n\n.Tooltip_core-tooltip-content__2TEir {\n  max-width: 180px;\n  border-radius: 3px;\n  background-color: #172b4d;\n  color: #fff;\n  padding: 10px 14px;\n  animation: Tooltip_anvil__2Vibk 0.3s cubic-bezier(0.38, 0.1, 0.36, 0.9) forwards; }\n";
-styleInject_es['default'](css_248z);

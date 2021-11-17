9aeb172ab60cd304b584b7463e41f6bb89618040
peggy.shi <peggys@innotech.me>
2021-11-17 17:09:08 +0800

    feat(UT-612): add progress component

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,9 @@ export type {
 export { default as Modal } from "./components/Modal"
 export type { ModalProps } from "./components/Modal"
 
+export { default as Progress } from "./components/Progress"
+export type { ProgressProps } from "./components/Progress"
+
 export { default as Select } from "./components/Select"
 export type { SelectProps, OptionType } from "./components/Select"
 

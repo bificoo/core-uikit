de194f3d543b8b99dc402935b27b2b3ae21c2819
wadezhu <wade.zhu@cloudlytics.me>
2022-01-12 22:53:52 +0800

    refactor(FE-8): modify rollup settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,34 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
-import del from "rollup-plugin-delete";
-import url from "@rollup/plugin-url"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "rollup-plugin-typescript2"
+import postcss from "rollup-plugin-postcss"
 import copy from "rollup-plugin-copy"
+import url from "@rollup/plugin-url"
 import svgr from "@svgr/rollup"
 import ttypescript from 'ttypescript'
+import del from "rollup-plugin-delete";
 
-const path = require("path");
+const path = require("path")
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   input: "src/index",
   output: {
-    name: "uikit",
     dir: "build",
     format: "cjs",
     exports: "named",
-    sourcemap: true,
-    globals: { react: 'React' },
+    preserveModules: true,
   },
-  preserveModules: true,
   plugins: [
-    url(),
-    svgr({ removeDimensions: true }),
     del({ targets: 'build/*' }),
-    copy({ targets: [{ src: "src/types/*", dest: "build/src/types" }] }),
+    peerDepsExternal(),
+    resolve({ extensions, browser: true, mainFields: ["module"] }),
     // Node.js 有些套件需要加入include設定才會正常
     commonjs({ include: ['node_modules/**'] }),
-    peerDepsExternal(),
     // https://github.com/zerkalica/zerollup/issues/11
-    typescript({ useTsconfigDeclarationDir: true, clean: true, typescript: ttypescript }),
-    resolve({ extensions, browser: true, mainFields: ["main"] }),
+    typescript({ typescript: ttypescript }),
     postcss({
       extract: false,
       modules: true,
@@ -47,6 +41,9 @@ export default {
         }],
         ["less", { javascriptEnabled: true }],
       ],
-    })
-  ]
-};
+    }),
+    url(),
+    svgr({ removeDimensions: true }),
+    copy({ targets: [{ src: "src/types/*", dest: "build/src/types" }] }),
+  ],
+}

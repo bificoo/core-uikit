80b6c711fbf69f51c08ddcd07fd286caa6dc3217
wadezhu <wade.zhu@cloudlytics.me>
2022-01-14 16:20:51 +0800

    build(UT-877): fix eslint error

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,9 +1,9 @@
 import Arrow from "./Arrow"
-export type { IconArrowProps } from "./Arrow"
 import Search from "./Search"
 import Group from "./Group"
 import User from "./User"
 import Information from "./Information"
+export type { IconArrowProps } from "./Arrow"
 
 const Icon = {
   Arrow,Search,Group,User,Information,

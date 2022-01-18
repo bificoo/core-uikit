068bbcf1cc15206ff8feed187fa06a8a5c6175eb
Sunny <sunny.huang@cloudlytics.me>
2022-01-18 18:07:59 +0800

    feat(UT-874): remove the unnecessary code

--- src/components/Form/FormHorizontal/index.tsx
+++ src/components/Form/FormHorizontal/index.tsx
@@ -1,6 +1,5 @@
 import { WithChildren } from "types/common"
 import styled from "./FormHorizontal.module.scss"
-import { WithChildren } from "types/common"
 
 const FormHorizontal = (props: WithChildren) => {
   return <div className={styled.wrapper}>{props.children}</div>

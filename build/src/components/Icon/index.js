007c89862ab8009dc04b65962fb56986b4e4d839
Sunny <sunny.huang@cloudlytics.me>
2022-02-14 18:02:41 +0800

    feat(FE-12): fixed the discussion on the gitlab

--- build/src/components/Icon/index.js
+++ build/src/components/Icon/index.js
@@ -1,6 +1,5 @@
 import { __assign } from '../../../node_modules/tslib/tslib.es6.js';
 import { jsx } from 'react/jsx-runtime';
-import { useMemo } from 'react';
 import Image from './svg/index.js';
 import cx from 'classnames';
 import styled from './Icon.module.scss.js';
@@ -9,29 +8,17 @@ var Icon = function (props) {
     var _a;
     var camelCased = props.type.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
     var compName = camelCased.charAt(0).toUpperCase() + camelCased.slice(1);
-    var filterIcon = useMemo(function () {
-        switch (compName) {
-            case 'ArrowUp':
-                return jsx(Image.ArrowUp, {}, void 0);
-            case 'ArrowDown':
-                return jsx(Image.ArrowDown, {}, void 0);
-            case 'ArrowLeft':
-                return jsx(Image.ArrowLeft, {}, void 0);
-            case 'ArrowRight':
-                return jsx(Image.ArrowRight, {}, void 0);
-            case 'Search':
-                return jsx(Image.Search, {}, void 0);
-            case 'Group':
-                return jsx(Image.Group, {}, void 0);
-            case 'User':
-                return jsx(Image.User, {}, void 0);
-            case 'Information':
-                return jsx(Image.Information, {}, void 0);
-            default:
-                return null;
-        }
-    }, [compName]);
-    return (jsx("div", __assign({ className: cx((_a = {}, _a[styled.arrowWrapper] = compName.includes("Arrow"), _a), props.className), style: __assign({ display: "flex" }, props.style) }, { children: filterIcon }), void 0));
+    var compareIcon = {
+        'ArrowUp': jsx(Image.ArrowUp, {}, void 0),
+        'ArrowDown': jsx(Image.ArrowDown, {}, void 0),
+        'ArrowLeft': jsx(Image.ArrowLeft, {}, void 0),
+        'ArrowRight': jsx(Image.ArrowRight, {}, void 0),
+        'Search': jsx(Image.Search, {}, void 0),
+        'Group': jsx(Image.Group, {}, void 0),
+        'User': jsx(Image.User, {}, void 0),
+        'Information': jsx(Image.Information, {}, void 0),
+    };
+    return (jsx("div", __assign({ className: cx((_a = {}, _a[styled.arrowWrapper] = compName.includes("Arrow"), _a), props.className), style: __assign({ display: "flex" }, props.style) }, { children: compareIcon[compName] }), void 0));
 };
 
 export { Icon, Icon as default };
--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react"
 import Image from "./svg"
 import cx from "classnames"
 import styled from "./Icon.module.scss"
@@ -8,32 +7,20 @@ export const Icon = (props: { type: string } & WithClassName & WithStyle) => {
   const camelCased = props.type.replace(/-([a-z])/g, g => g[1].toUpperCase())
   const compName = camelCased.charAt(0).toUpperCase() + camelCased.slice(1)
 
-  const filterIcon = useMemo(() => {
-    switch (compName) {
-      case 'ArrowUp':
-        return <Image.ArrowUp />
-      case 'ArrowDown':
-        return <Image.ArrowDown />
-      case 'ArrowLeft':
-        return <Image.ArrowLeft />
-      case 'ArrowRight':
-        return <Image.ArrowRight />
-      case 'Search':
-        return <Image.Search />
-      case 'Group':
-        return <Image.Group />
-      case 'User':
-        return <Image.User />
-      case 'Information':
-        return <Image.Information />
-      default:
-        return null
-    }
-  }, [compName])
+  const compareIcon: { [key: string]: JSX.Element } = {
+    'ArrowUp': <Image.ArrowUp />,
+    'ArrowDown': <Image.ArrowDown />,
+    'ArrowLeft': <Image.ArrowLeft />,
+    'ArrowRight': <Image.ArrowRight />,
+    'Search': <Image.Search />,
+    'Group': <Image.Group />,
+    'User': <Image.User />,
+    'Information': <Image.Information />,
+  }
 
   return (
     <div className={cx({ [styled.arrowWrapper]: compName.includes("Arrow") }, props.className)} style={{ display: "flex", ...props.style }}>
-      {filterIcon}
+      {compareIcon[compName]}
     </div>
   )
 }

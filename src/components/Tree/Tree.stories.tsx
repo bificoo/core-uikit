e3136c2afb9204413ccaeae3a961bb7db8cb6bfa
Sunny <sunny.huang@cloudlytics.me>
2022-02-15 15:13:21 +0800

    fix(UT-964): add the example for delay setting selected and expanded key

--- src/components/Tree/Tree.stories.tsx
+++ src/components/Tree/Tree.stories.tsx
@@ -94,3 +94,49 @@ export const SpecifyKey: Story = () => {
     </div>
   )
 }
+
+export const DelaySetSelectedKey: Story = () => {
+  const [expanded, setExpanded] = useState<string[]>([])
+  const [selected, setSelected] = useState<string[]>([])
+
+  const handleSelect = (nodeIds: string[]) => {
+    setSelected(nodeIds)
+  }
+
+  const handleToggle = (nodeIds: string[]) => {
+    setExpanded(nodeIds)
+  }
+
+  setTimeout(() => {
+    setSelected(["GE", "GE01", "BetPreferHour"])
+    setExpanded(["GE", "GE01"])
+  }, 3000)
+
+  return (
+    <div style={{ width: "240px" }}>
+      <Tree
+        expanded={expanded}
+        selected={selected}
+        onNodeToggle={handleToggle}
+        onNodeSelect={handleSelect}>
+        {fakeData.map(theme => (
+          <Tree.Category
+            key={theme.id}
+            renderName={`${theme.name}(${theme.count})`}
+            eventKey={theme.id}>
+            {theme.category.map(category => (
+              <Tree.Category
+                key={category.id}
+                renderName={`${category.name}(${category.count})`}
+                eventKey={category.id}>
+                {category.labels.map(label => (
+                  <Tree.Item key={label.id} renderName={label.name} eventKey={label.id} />
+                ))}
+              </Tree.Category>
+            ))}
+          </Tree.Category>
+        ))}
+      </Tree>
+    </div>
+  )
+}

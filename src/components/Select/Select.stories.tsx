9ecd51c608ef2d808b508ff770b8a979996088e4
Pearl Yeh <pearl.yeh@cloudlytics.me>
2022-06-07 14:32:10 +0800

    feat(FE-85): fix select story sorting

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -8,104 +8,66 @@ export default {
   argTypes: {},
 } as Meta
 
-export const Single: Story<SelectProps> = () => {
-  const options = [
-    { value: "Vanilla", eventKey: "vanilla" },
-    { value: "Strawberry", eventKey: "strawberry" },
-    { value: "Chocolate", eventKey: "chocolate" },
-    { value: "Mango", eventKey: "mango" },
-    { value: "Passionfruit", eventKey: "passionfruit" },
-    { value: "Hazelnut", eventKey: "hazelnut" },
-    { value: "Durian", eventKey: "durian" },
-  ]
+const options = [
+  { value: "Vanilla", eventKey: "vanilla" },
+  { value: "Strawberry", eventKey: "strawberry" },
+  { value: "Chocolate", eventKey: "chocolate" },
+  { value: "Mango", eventKey: "mango" },
+  { value: "PassionFruit", eventKey: "passionFruit" },
+  { value: "Hazelnut", eventKey: "hazelnut" },
+  { value: "Durian", eventKey: "durian" },
+  { value: "Apple", eventKey: "apple" },
+  { value: "Banana", eventKey: "banana" },
+  { value: "Orange", eventKey: "orange" },
+  { value: "Cherry", eventKey: "cherry" },
+  { value: "Blueberry", eventKey: "blueberry" },
+]
 
-  return (
-    <Select defaultValue={options[0]} placeholder="Select..">
+export const Default: Story<SelectProps> = () => (
+  <Select defaultValue={[options[0]]} placeholder="Select..">
+    {options.map(option => (
+      <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
+        {option.value}
+      </Select.Option>
+    ))}
+  </Select>
+)
+
+export const Multiple: Story<SelectProps> = () => (
+  <div style={{ display: "flex" }}>
+    <Select
+      isMulti
+      defaultValue={[options[0], options[1]]}
+      placeholder="Select.."
+      onChange={(e, value) => console.log("onChange", value)}>
       {options.map(option => (
         <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
           {option.value}
         </Select.Option>
       ))}
     </Select>
-  )
-}
+  </div>
+)
 
-export const Multiple: Story<SelectProps> = () => {
-  const options = [
-    { value: "Vanilla", eventKey: "vanilla" },
-    { value: "Strawberry", eventKey: "strawberry" },
-    { value: "Chocolate", eventKey: "chocolate" },
-    { value: "Mango", eventKey: "mango" },
-    { value: "Passionfruit", eventKey: "passionfruit" },
-    { value: "Hazelnut", eventKey: "hazelnut" },
-    { value: "Durian", eventKey: "durian" },
-  ]
+export const Scroll: Story<SelectProps> = () => (
+  <Select placeholder="Select..">
+    {options.map(option => (
+      <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
+        {option.value}
+      </Select.Option>
+    ))}
+  </Select>
+)
 
-  return (
-    <div style={{ display: "flex" }}>
-      <Select
-        isMulti
-        defaultValue={[options[0], options[1]]}
-        placeholder="Select.."
-        onChange={(e, value) => console.log("onChange", value)}>
-        {options.map(option => (
-          <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
-            {option.value}
-          </Select.Option>
-        ))}
-      </Select>
-    </div>
-  )
-}
-
-export const Scroll: Story<SelectProps> = () => {
-  const options = [
-    { value: "Vanilla", eventKey: "vanilla" },
-    { value: "Strawberry", eventKey: "strawberry" },
-    { value: "Chocolate", eventKey: "chocolate" },
-    { value: "Mango", eventKey: "mango" },
-    { value: "Passionfruit", eventKey: "passionfruit" },
-    { value: "Hazelnut", eventKey: "hazelnut" },
-    { value: "Durian", eventKey: "durian" },
-    { value: "Apple", eventKey: "apple" },
-    { value: "Banana", eventKey: "banana" },
-    { value: "Orange", eventKey: "orange" },
-    { value: "Cherry", eventKey: "cherry" },
-    { value: "Blueberry", eventKey: "blueberry" },
-  ]
-
-  return (
-    <Select placeholder="Select..">
-      {options.map(option => (
-        <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
-          {option.value}
-        </Select.Option>
-      ))}
-    </Select>
-  )
-}
-
-export const Disabled: Story<SelectProps> = () => {
-  const options = [
-    { value: "Vanilla", eventKey: "vanilla" },
-    { value: "Strawberry", eventKey: "strawberry" },
-    { value: "Chocolate", eventKey: "chocolate" },
-    { value: "Mango", eventKey: "mango" },
-    { value: "Passionfruit", eventKey: "passionfruit" },
-    { value: "Hazelnut", eventKey: "hazelnut" },
-    { value: "Durian", eventKey: "durian" },
-  ]
-
-  return (
-    <Select isMulti defaultValue={[options[0], options[1]]} disabled>
-      {options.map(option => (
-        <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
-          {option.value}
-        </Select.Option>
-      ))}
-    </Select>
-  )
-}
+export const Disabled: Story<SelectProps> = () => (
+  <Select isMulti defaultValue={[options[0], options[1]]} disabled>
+    {options.map(option => (
+      <Select.Option key={option.value} value={option.value} eventKey={option.eventKey}>
+        {option.value}
+      </Select.Option>
+    ))}
+  </Select>
+)
 
 export const ValidationError: Story<SelectProps> = () => {
   const options = [
